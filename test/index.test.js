a8076b0758636cb8915c34d45fc93af043c12a9a
JuFeng Zhang <zjffun@gmail.com>
2020-10-22 19:36:14 +0800

    feat: add support of setting custom metadata

--- test/index.test.js
+++ test/index.test.js
@@ -8,10 +8,12 @@ const metadata = require('../src/');
 
 const fixturesDir = path.join(__dirname, '/fixtures');
 const runtimeDir = path.join(__dirname, '/runtime');
-const remark = unified().use(parse).use(stringify).use(frontmatter).freeze();
+const remark = unified().use(parse).use(stringify).use(frontmatter)
+  .freeze();
 
-// Utility function to add metdata to a vFile.
+// Utility function to add metadata to a vFile.
 function addMetadata(vFile, destinationFilePath) {
+  // eslint-disable-next-line no-param-reassign
   vFile.data = {
     destinationFilePath,
     destinationDir: path.dirname(destinationFilePath),
@@ -19,44 +21,182 @@ function addMetadata(vFile, destinationFilePath) {
 }
 
 describe('remark-metadata', () => {
-  it('adds metadata to existing front matter', () => {
-    const srcFile = `${fixturesDir}/existing.md`;
-    const destFile = `${runtimeDir}/existing.md`;
-    const vfile = toVFile.readSync(srcFile);
-    addMetadata(vfile, destFile);
+  describe('src file name with spaces should work fine', () => {
+    const srcFile = `${fixturesDir}/file name with spaces.md`;
+    const destFile = `${runtimeDir}/file name with spaces.md`;
+    let vfile;
 
-    const result = remark().use(metadata).processSync(vfile).toString();
-    expect(result).toContain('lastModifiedAt:');
+    beforeEach(() => {
+      vfile = toVFile.readSync(srcFile);
+      addMetadata(vfile, destFile);
+    });
+
+    it('when using git', () => {
+      const result = remark()
+        .use(frontmatter)
+        .use(metadata, {
+          metadata: {
+            created: metadata.GIT_CREATED_TIME,
+            updated: metadata.GIT_LAST_MODIFIED_TIME,
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      // should be "created: 'DATE'"
+      expect(result).not.toContain("created: ''");
+
+      // should be "updated: 'DATE'"
+      expect(result).not.toContain("updated: ''");
+    });
+
+    it('when using stat', () => {
+      const result = remark()
+        .use(metadata, {
+          metadata: {
+            updated: metadata.LAST_MODIFIED_TIME,
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      // should be "created: 'DATE'"
+      expect(result).not.toContain("created: ''");
+
+      // should be "updated: 'DATE'"
+      expect(result).not.toContain("updated: ''");
+    });
   });
 
-  it('adds metadata using stat', () => {
+  describe('adds metadata to existing front matter', () => {
     const srcFile = `${fixturesDir}/existing.md`;
     const destFile = `${runtimeDir}/existing.md`;
-    const vfile = toVFile.readSync(srcFile);
-    addMetadata(vfile, destFile);
+    let vfile;
 
-    const result = remark().use(metadata, { git: false }).processSync(vfile).toString();
-    expect(result).toContain('lastModifiedAt:');
+    beforeEach(() => {
+      vfile = toVFile.readSync(srcFile);
+      addMetadata(vfile, destFile);
+    });
+
+    it('when provide shouldUpdate option', () => {
+      const mockFn = jest.fn();
+      const result = remark()
+        .use(metadata, {
+          metadata: {
+            lastModifiedAt: {
+              value: metadata.LAST_MODIFIED_TIME,
+              shouldUpdate(newValue, oldValue) {
+                mockFn();
+                expect(newValue).not.toBeFalsy();
+                expect(oldValue).toBe('Thu, 22 Oct 2020 06:47:56 GMT');
+
+                return false;
+              },
+            },
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      expect(mockFn).toHaveBeenCalledTimes(1);
+      expect(result).toContain("lastModifiedAt: 'Thu, 22 Oct 2020 06:47:56 GMT'");
+    });
+
+    it('when does not provide shouldUpdate option', () => {
+      const result = remark()
+        .use(metadata, {
+          metadata: {
+            lastModifiedAt: {
+              value: metadata.LAST_MODIFIED_TIME,
+            },
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      expect(result).not.toContain('lastModifiedAt: Thu, 22 Oct 2020 06:47:56 GMT');
+    });
+
+    it('when provide gitExcludeCommit option', () => {
+      const result = remark()
+        .use(metadata, {
+          gitExcludeCommit: '',
+          metadata: {
+            lastModifiedAt: metadata.GIT_LAST_MODIFIED_TIME,
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      expect(result).toContain("lastModifiedAt: ''");
+    });
+
+    it('when provide string metadata', () => {
+      const result = remark()
+        .use(metadata, {
+          metadata: {
+            foo: 'bar',
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      expect(result).toContain('foo: bar');
+    });
+
+    it('when provide function metadata', () => {
+      const mockFn = jest.fn();
+      const result = remark()
+        .use(metadata, {
+          metadata: {
+            foo({
+              gitModifiedTime,
+              gitCreatedTime,
+              modifiedTime,
+              vFile,
+              oldFrontMatter,
+            }) {
+              mockFn();
+              expect(gitModifiedTime).toBeTruthy();
+              expect(gitCreatedTime).toBeTruthy();
+              expect(modifiedTime).toBeTruthy();
+              expect(vFile).toBeTruthy();
+              expect(oldFrontMatter).toBeTruthy();
+              return 'bar';
+            },
+          },
+        })
+        .processSync(vfile)
+        .toString();
+
+      expect(mockFn).toHaveBeenCalledTimes(1);
+      expect(result).toContain('foo: bar');
+    });
   });
 
-  it('adds metadata as new front matter', () => {
+  describe('adds metadata as new front matter', () => {
     const srcFile = `${fixturesDir}/no.md`;
     const destFile = `${runtimeDir}/no.md`;
-    const vfile = toVFile.readSync(srcFile);
-    addMetadata(vfile, destFile);
+    let vfile;
 
-    const result = remark().use(metadata).processSync(vfile).toString();
-    expect(result).toContain('lastModifiedAt:');
-  });
+    beforeEach(() => {
+      vfile = toVFile.readSync(srcFile);
+      addMetadata(vfile, destFile);
+    });
 
-  it('reuses modified date from vFile data', () => {
-    const srcFile = `${fixturesDir}/existing.md`;
-    const destFile = `${runtimeDir}/existing.md`;
-    const vfile = toVFile.readSync(srcFile);
-    addMetadata(vfile, destFile);
-    vfile.data.lastModifiedAt = 'Tue, 28 Nov 2017 02:44:25 GMT';
+    it('when provide metadata', () => {
+      const result = remark()
+        .use(metadata, {
+          metadata: { lastModifiedAt: metadata.LAST_MODIFIED_TIME },
+        })
+        .processSync(vfile)
+        .toString();
+      expect(result).toContain('lastModifiedAt:');
+    });
 
-    const result = remark().use(metadata).processSync(vfile).toString();
-    expect(result).toContain('lastModifiedAt:');
+    it('when does not provide metadata', () => {
+      const result = remark().use(metadata).processSync(vfile).toString();
+      expect(result).toContain('{}');
+    });
   });
 });
